0df3dfbfdaf3da61c2513fb9f9655dfb2090acaf
Christian G. <christian.goebel@stud.tu-darmstadt.de>
2024-12-15 17:11:43 +0100

    Preliminary Board

--- Assets/Scripts/TRB Script.cs
+++ Assets/Scripts/TRB Script.cs
@@ -1,11 +1,15 @@
+using Oculus.Interaction;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TRBScript : MonoBehaviour
 {
+    public float throwingSpeed;
     public float maxSpeed;
+    public float gravityMultiplier;
     public float maxRange;
     public int nrOldPositions;
+    public Transform leftHandTransform;
 
     public GameObject Player;
 
@@ -13,7 +17,11 @@ public class TRBScript : MonoBehaviour
     private Rigidbody rb;
     private Queue<Vector3> oldPositions = new();
     private bool isCurrentlyGrabbed;
-    //private bool isGrabbing =>;
+    
+    private bool isGrabbing => (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > .1f
+                            || OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > .1f)
+                            && true;//object is in hand - via collider?
+    private Transform Transform => GetComponent<Transform>();
 
     void Start()
     {
@@ -25,39 +33,45 @@ public class TRBScript : MonoBehaviour
     void Update()
     {
         //calculate control inputs - grab, teleport, return
-        //bool letGo = isCurrentlyGrabbed && !isGrabbing;
-        //isCurrentlyGrabbed = isGrabbing;
-        //bool doTeleport = interaction.button;
-        //bool doReturn = interaction.button;
+        bool letGo = isCurrentlyGrabbed && !isGrabbing;
+        isCurrentlyGrabbed = isGrabbing;
+        bool doTeleport = OVRInput.GetDown(OVRInput.Button.One);
+        bool doReturn = OVRInput.Get(OVRInput.Button.Three);
 
-        //update position queue
-        //if (isGrabbing) 
-        //{
-        Transform transform = GetComponent<Transform>();
-        oldPositions.Enqueue(transform.position);
-        if (oldPositions.Count > nrOldPositions) oldPositions.Dequeue();
-        //}
+
 
         //resolve let go
-        //if (letGo)
-        //{
-        //    Vector3 movement = transform.position - oldPositions.Peek();
-        //    //rb.AddForce(movement);
-        //    oldPositions.Clear();
-        //}
+        if (letGo)
+        {
+            Vector3 movement = (transform.position - oldPositions.Peek()) * throwingSpeed;
+            if (movement.magnitude > maxSpeed) movement = movement.normalized * maxSpeed;
+            Debug.LogWarning("Throw with movement:" + movement.ToString());
+            rb.AddForce(movement, ForceMode.VelocityChange);
+            //rb.angularVelocity = rotationfromtransformaswithmovement
+        }
 
         //resolve return to hand
-        //if (doReturn)
-        //{
-        //    //maybe there's a way to directly put something into a grab?
-        //    //if not, put the board below the player and the player on top
-        //}
+        if (doReturn)
+        {
+            transform.position = leftHandTransform.position + leftHandTransform.right * .1f;
+        }
 
         //resolve teleport player
-        //if (doTeleport)
-        //{
-        //    Player.transform.position = transform.position + new Vector3(0, Player.transform.position.y, 0);
-        //    //maybe put the board into the right hand
-        //}
+        if (doTeleport)
+        {
+            Player.transform.position = transform.position + new Vector3(0, -.04f, 0);
+            //maybe put the board into the right hand
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isGrabbing)
+        {
+            rb.AddForce(Vector3.down * gravityMultiplier, ForceMode.Force);
+        }
+        //update position queue (don't care about delta)
+        oldPositions.Enqueue(Transform.position);
+        if (oldPositions.Count > nrOldPositions) oldPositions.Dequeue();
     }
 }
